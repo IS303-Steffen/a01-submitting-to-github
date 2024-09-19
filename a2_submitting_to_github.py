8c5c4be4bfbc39117244bd3a599524d9e38b246c
jacobsteffenBYU <jacob.steffen@byu.edu>
2024-09-19 11:32:44 -0600

    updated main py file comment width

--- a2_submitting_to_github.py
+++ a2_submitting_to_github.py
@@ -1,7 +1,11 @@
-# Name:
-# Description: Enter your code below.
-# You can test your code using the testing tab on the left.
-# When finished, upload your code by going to the source control tab,
-# committing your code, and then syncing it.
-# Replace this text with your own description of what your code does.
+# Name: <Enter your name>
+# Description: <Enter a 1 -2 sentence description of your code>
+'''
+Write your code below these comments.
 
+You can test your code using the testing tab on the left.
+When finished, upload your code by going to the source control tab,
+committing your code, and then syncing it.
+
+You can delete these comments after reading them.
+'''
