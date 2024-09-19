9ff69c8ba963ba2eb55632c0ef178a38d2307fbb
jacobsteffenBYU <jacob.steffen@byu.edu>
2024-09-19 11:32:25 -0600

    updated main py file for pep-8 width guidelines.

--- a2_submitting_to_github.py
+++ a2_submitting_to_github.py
@@ -1,5 +1,7 @@
 # Name:
-# Description:  Enter your code below. You can test your code using the testing tab on the left.
-#               When finished, upload your code by going to the source control tab, committing your code, and then syncing it.
-#               Replace this text with your own description of what your code does.
+# Description: Enter your code below.
+# You can test your code using the testing tab on the left.
+# When finished, upload your code by going to the source control tab,
+# committing your code, and then syncing it.
+# Replace this text with your own description of what your code does.
 
